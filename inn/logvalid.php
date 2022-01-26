6ff475a9b2ce8495ae1a13afc07aec764992e06b
Prasad W <88481022+MrPrasaD69@users.noreply.github.com>
2022-01-26 19:56:13 +0530

    Add files via upload
    PHP Login and Registration

---
+++ inn/logvalid.php
@@ -0,0 +1,36 @@
+<?php
+
+include('dbcon.php');
+
+
+$con = mysqli_connect('localhost','root','');
+if($con)
+{
+    echo "Connected";
+}
+else
+{
+    echo "Not Connected";
+}
+mysqli_select_db($con, 'students' );
+
+$name=$_POST['username'];
+$pass=$_POST['password'];
+
+//checking table Uname/Upassword 
+$q="select * from table1 where Uname = '$name' && Upassword= '$pass' ";
+
+$result= mysqli_query($con, $q);
+$num=mysqli_num_rows($result);
+if($num==1)
+{
+	$_SESSION['Susername']= $name;
+        echo "Log in success";
+        header('location:index.php');
+}
+else
+{
+    echo '<script>alert("Invalid Credentials")</script>';
+}
+
+?>
---
+++ inn/register.php
@@ -0,0 +1,35 @@
+
+<html lang="en_US">
+    <head>
+        <meta charset ="UTF-8">
+        <title> User Registration </title>
+        
+    </head>
+    <body>
+
+<h1 align="center">User Register</h1><br>
+        <form action="regvalid.php" method="post">
+            
+            <table align="center">
+                <tr>
+                    <td>Username</td><td><input type="text" name="username1"></td>
+                </tr>
+                <tr>
+                    <td>Email</td><td><input type="email" name="email"></td>
+                </tr>
+                <tr>
+                    <td>Phone</td><td><input type="text" name="phone"></td>
+                </tr>
+                <tr>
+                    <td>Password</td><td><input type="password" name="password1"></td>
+                </tr>
+                <tr>
+                    <td>Address</td><td><input type="text" name="address"></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" name="register" value="Register"></td>
+                </tr>
+                
+            </table>
+            
+        </form>
