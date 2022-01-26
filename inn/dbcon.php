d94076963087385af39c68aef9257b0312f40a8b
Prasad W <88481022+MrPrasaD69@users.noreply.github.com>
2022-01-26 20:09:10 +0530

    Add files via upload
    Login, Registration and Session with PHP

---
+++ inn/dbcon.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="students";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+  echo " Connection failed";
+}
+echo "Connected successfully";
+?>
